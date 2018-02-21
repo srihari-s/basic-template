42d60a88e9b4f52b7deb97001c49ff9b5dbce4b9
srihari-s <srihari.developer1@gmail.com>
2018-02-21 11:08:22 +0530

    updated dao model

--- src/main/java/org/learning/basic_template/dao/GenericDao.java
+++ src/main/java/org/learning/basic_template/dao/GenericDao.java
@@ -3,12 +3,20 @@ package org.learning.basic_template.dao;
 import java.util.List;
 
 import org.learning.basic_template.model.Auditable;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
-public interface GenericDao<K,E extends Auditable<K>> {
+public interface GenericDao<K extends Long,E extends Auditable<K>> {
 
 	K save(E e);
 	E findOne(K k);
 	List<E> getAll();
 	boolean delete(E e);
 	String getTableName();
+	String getFindOneQuery();
+	String getFindAllQuery();
+	String getDeleteQuery();
+	SqlParameterSource getSqlParameterSource(E e);
+	RowMapper getRowMapper();
+
 }
--- src/main/java/org/learning/basic_template/dao/GenericDaoImpl.java
+++ src/main/java/org/learning/basic_template/dao/GenericDaoImpl.java
@@ -1,45 +1,65 @@
 package org.learning.basic_template.dao;
 
-import java.util.List;
-
 import org.learning.basic_template.model.Auditable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
-public abstract class GenericDaoImpl implements GenericDao<Long, Auditable<Long>> {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-	@Autowired
-	NamedParameterJdbcTemplate nJdbcTemplate;
-	
-	@Override
-	public Long save(Auditable<Long> e) {
-		// TODO Auto-generated method stub
-	
-		SimpleJdbcInsert simpleInsert = new SimpleJdbcInsert(nJdbcTemplate.getJdbcTemplate()).withTableName(getTableName());
-		simpleInsert.setGeneratedKeyName("ID");
-		//simpleInsert.executeAndReturnKey(parameterSource);
-		return null;
-	}
+public abstract class GenericDaoImpl implements GenericDao {
 
-	@Override
-	public Auditable<Long> findOne(Long k) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Autowired
+    NamedParameterJdbcTemplate nJdbcTemplate;
 
-	@Override
-	public List<Auditable<Long>> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public boolean delete(Auditable<Long> e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public Long save(Auditable auditable) {
+        SimpleJdbcInsert simpleInsert = new SimpleJdbcInsert(nJdbcTemplate.getJdbcTemplate()).withTableName(getTableName());
+        simpleInsert.setGeneratedKeyName("ID");
+        Number id = simpleInsert.executeAndReturnKey(getSqlParameterSource(auditable));
+        return id.longValue();
+    }
 
-	
-	
+    @Override
+    public Auditable findOne(Long aLong) {
+        Map map = new HashMap<String,String>(1);
+        map.put("id", aLong);
+        return (Auditable)nJdbcTemplate.queryForObject(getFindOneQuery(), map, getRowMapper());
+    }
+
+    @Override
+    public List<Auditable> getAll() {
+        return nJdbcTemplate.query(getFindAllQuery(), getRowMapper());
+    }
+
+    @Override
+    public boolean delete(Auditable e) {
+        Map map = new HashMap<String,String>(1);
+        map.put("id", e.getId());
+        int count = nJdbcTemplate.update(getDeleteQuery(),map);
+        if (1 == count)
+            return true;
+        return false;
+    }
+
+
+    @Override
+    public String getFindOneQuery() {
+        return new StringBuilder("SELECT * FROM ").append(getTableName()).append(" WHERE ID:id").toString();
+    }
+
+    @Override
+    public String getDeleteQuery() {
+        return new StringBuilder("DELETE * FROM ").append(getTableName()).append(" WHERE ID:id").toString();
+    }
+
+    @Override
+    public String getFindAllQuery() {
+        return new StringBuilder("SELECT * FROM ").append(getTableName()).toString();
+    }
+
+
 }
